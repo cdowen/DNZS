109f99f634188865c50dfb6baa32048488a8e67e
cdowen <hhylx1950@gmail.com>
2013-01-09 13:18:06 +0800

    Just try KDevelop

--- sample/num.php
+++ sample/num.php
@@ -1,10 +1,10 @@
 <?php
 $time = $_GET['date'];
-$conn = mysql_connect('localhost', 'dnzsuser', '123456');
-mysql_select_db('DNZS');
 if (!is_numeric($time))
 	die();
+$conn = mysql_connect('localhost', 'dnzsuser', '123456');
+mysql_select_db('DNZS');
 $result = mysql_query("SELECT `time` FROM `appointment` WHERE time<=$time");
-$sum= mysql_num_rows($result);
+$sum = mysql_num_rows($result);
 echo $sum;
 ?>
